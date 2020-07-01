8d55d76830bafbfcfc0a418c133c57b0b497096f
Craig McKeachie <craigdmckeachie@gmail.com>
2020-07-01 16:22:04 -0400

    lab12

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -7,19 +7,46 @@ interface ProjectListProps {
   projects: Project[];
 }
 
-class ProjectList extends React.Component<ProjectListProps> {
-  handleEdit = (project: Project) => {
-    console.log(project);
+interface ProjectListState {
+  editingProject: Project | {};
+}
+
+class ProjectList extends React.Component<ProjectListProps, ProjectListState> {
+  state = {
+    editingProject: {},
   };
+  handleEdit = (editingProject: Project) => {
+    this.setState({ editingProject: editingProject });
+  };
+
   render() {
     const { projects } = this.props;
-    const items = projects.map((project) => (
-      <div key={project.id} className="cols-sm">
-        <ProjectCard onEdit={this.handleEdit} project={project}></ProjectCard>
-        <ProjectForm />
-      </div>
-    ));
+
+    let item: JSX.Element;
+    const items = projects.map((project: Project) => {
+      if (project !== this.state.editingProject) {
+        item = (
+          <div key={project.id} className="cols-sm">
+            <ProjectCard
+              project={project}
+              onEdit={() => {
+                this.handleEdit(project);
+              }}
+            ></ProjectCard>
+          </div>
+        );
+      } else {
+        item = (
+          <div key={project.id} className="cols-sm">
+            <ProjectForm></ProjectForm>
+          </div>
+        );
+      }
+      return item;
+    });
+
     return <div className="row">{items}</div>;
   }
 }
+
 export default ProjectList;
