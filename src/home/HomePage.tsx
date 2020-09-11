f75578ee0e3f46fd9032980676877be839d0ae94
Craig McKeachie <craigdmckeachie@gmail.com>
2020-09-11 14:06:15 -0400

    add content to homepage

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
 export default function HomePage() {
-  return <h2>Home</h2>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: 'center',
+        textAlign: 'center',
+        width: '100%',
+      }}
+    >
+      <h1>The All-In-One Toolkit for Working Remotely.</h1>
+      <h3>
+        Anything and everything, regardless of unimportance! The experiences
+        factor is social-network-based. The interfaces factor is plug-and-play,
+        blog-based. What does it really mean to evolve "strategically"? What do
+        we scale? Anything and everything, regardless of reconditeness! What
+        does the commonly-used standard industry commonly-accepted term "project
+        management" really mean?
+      </h3>
+
+      <button className="button large primary">Give it a Try</button>
+
+      <blockquote cite="Dale Behetam, Easy Rentals" className="doc">
+        We can see exactly what needs to be done and when. We're much more
+        efficient.
+      </blockquote>
+    </div>
+  );
 }
